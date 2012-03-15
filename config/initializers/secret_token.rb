2228820203b4e3bd4755781718ee1f03f24f84d5
burglar1989 <burglar1989@gmail.com>
2012-03-15 21:50:42 +0800

    first edition

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chihuo::Application.config.secret_token = 'ff1e3e4926a76e462f1046d06cb5ae08db8919507ca18027ca8722efcc895e09551ed63d167fd9952126aaae346b4fa0f858706d6e40c86e087691138be656da'
